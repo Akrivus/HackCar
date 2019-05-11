c069a455741ee7a52f6288ec7c4d482d36545cfc
Owen Adrian <akrivus@gmail.com>
2019-05-11 15:46:13 -0500

    Fixed bugs.

--- socket-client.py
+++ socket-client.py
@@ -1,5 +1,6 @@
 import keyboard
 import socket
+import time
 
 ipaddr = input("Enter IP address: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,14 +9,20 @@ while 1 > 0:
     if keyboard.is_pressed('w'):
         if keyboard.is_pressed('a'):
             client.sendto("turn ++".encode('utf-8'), (ipaddr, 4404))
+            time.sleep(0.1)
         elif keyboard.is_pressed('d'):
             client.sendto("turn +-".encode('utf-8'), (ipaddr, 4404))
+            time.sleep(0.1)
         else:
             client.sendto("drive +".encode('utf-8'), (ipaddr, 4404))
+            time.sleep(0.1)
     elif keyboard.is_pressed('s'):
         if keyboard.is_pressed('a'):
             client.sendto("turn -+".encode('utf-8'), (ipaddr, 4404))
+            time.sleep(0.1)
         elif keyboard.is_pressed('d'):
             client.sendto("turn --".encode('utf-8'), (ipaddr, 4404))
+            time.sleep(0.1)
         else:
             client.sendto("drive -".encode('utf-8'), (ipaddr, 4404))
+            time.sleep(0.1)
