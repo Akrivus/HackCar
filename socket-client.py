e06ff5762066006c6c384ae147ff4fc8e644fae6
Owen Adrian <akrivus@gmail.com>
2019-05-11 16:55:48 -0500

    Added more debug messages.

--- socket-client.py
+++ socket-client.py
@@ -2,28 +2,29 @@ import keyboard
 import socket
 import time
 
-ipaddr = input("Enter IP address: ")
+ipaddr = (input("Enter IP address: "), 9909)
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto("brake !".encode('utf-8'), (ipaddr, 9909))
+client.settimeout(1)
+client.sendto(b"brake !", ipaddr)
 
 while 1 > 0:
     if keyboard.is_pressed('w'):
         if keyboard.is_pressed('a'):
-            client.sendto("turn ++".encode('utf-8'), (ipaddr, 9909))
+            client.sendto(b"turn ++", ipaddr)
             time.sleep(0.1)
         elif keyboard.is_pressed('d'):
-            client.sendto("turn +-".encode('utf-8'), (ipaddr, 9909))
+            client.sendto(b"turn +-", ipaddr)
             time.sleep(0.1)
         else:
-            client.sendto("drive +".encode('utf-8'), (ipaddr, 9909))
+            client.sendto(b"drive +", ipaddr)
             time.sleep(0.1)
     elif keyboard.is_pressed('s'):
         if keyboard.is_pressed('a'):
-            client.sendto("turn -+".encode('utf-8'), (ipaddr, 9909))
+            client.sendto(b"turn -+", ipaddr)
             time.sleep(0.1)
         elif keyboard.is_pressed('d'):
-            client.sendto("turn --".encode('utf-8'), (ipaddr, 9909))
+            client.sendto(b"turn --", ipaddr)
             time.sleep(0.1)
         else:
-            client.sendto("drive -".encode('utf-8'), (ipaddr, 9909))
+            client.sendto(b"drive -", ipaddr)
             time.sleep(0.1)
--- socket-server.py
+++ socket-server.py
@@ -6,24 +6,25 @@ server.bind((input("Enter IP address: "), 9909))
 
 while 1 > 0:
     data, addr = server.recvfrom(7)
+    print("{}: Data received...".format(addr))
     if data == 'turn ++':
-        print("{}: Turn left!".format(addr))
+        print("\tTurn left!")
         os.system('''sh ./set-car-vector.sh w a''')
     if data == 'turn +-':
-        print("{}: Turn right!".format(addr))
+        print("\tTurn rigth!")
         os.system('''sh ./set-car-vector.sh w d''')
     if data == 'turn -+':
-        print("{}: Turn left!".format(addr))
+        print("\tTurn left!")
         os.system('''sh ./set-car-vector.sh s a''')
     if data == 'turn --':
-        print("{}: Turn right!".format(addr))
+        print("\tTurn right!")
         os.system('''sh ./set-car-vector.sh s d''')
     if data == 'drive +':
-        print("{}: Go forwards!".format(addr))
+        print("\tGo foward!")
         os.system('''sh ./set-car-vector.sh w _''')
     if data == 'drive -':
-        print("{}: Go backwards!".format(addr))
+        print("\tGo backward!")
         os.system('''sh ./set-car-vector.sh s _''')
     if data == 'brake !':
-        print("{}: Brake!".format(addr))
+        print("\tBrake!")
         os.system('''sh ./set-car-vector.sh _ _''')
