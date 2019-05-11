7df10c112360c42150ae3690dd2209e8bccff883
Owen Adrian <akrivus@gmail.com>
2019-05-11 17:19:08 -0500

    Updated car vector command.

--- set-car-vector.sh
+++ set-car-vector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat ./states/"$1$2".wav | csdr convert_i16_f | csdr gain_ff -3 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.148e3 -s 48000
+sox ./states/"$1$2".wav -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff -3 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.148e3 -s 48000
--- socket-server.py
+++ socket-server.py
@@ -2,6 +2,7 @@ import socket
 import os
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((input("Enter IP address: "), 9909))
 
 while 1 > 0:
