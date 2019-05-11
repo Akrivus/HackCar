1115cab64028d07bd8a826b8f58ad530333d3e43
Owen Adrian <akrivus@gmail.com>
2019-05-11 14:44:23 -0500

    Adjusting units and timing.

--- set-car-vector.sh
+++ set-car-vector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(while true; do cat ./states/"$1$2".wav; done) | csdr convert_i16_f | csdr gain_ff -3 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27148000 -s 48000
+cat ./states/"$1$2".wav | csdr convert_i16_f | csdr gain_ff -3 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.148e3 -s 48000
