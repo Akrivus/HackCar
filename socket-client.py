3cd8efe405e5444cabba0360b64b497646b5f911
Owen Adrian <akrivus@gmail.com>
2019-05-11 17:29:48 -0500

    Added more key delay.

--- socket-client.py
+++ socket-client.py
@@ -9,23 +9,23 @@ client.sendto(b"brake !", ipaddr)
 
 while 1 > 0:
     if keyboard.is_pressed('space'):
-            client.sendto(b"brake !", ipaddr)
-            time.sleep(0.1)
+        client.sendto(b"brake !", ipaddr)
+        time.sleep(1.0)
     elif keyboard.is_pressed('w'):
-            client.sendto(b"drive +", ipaddr)
-            time.sleep(0.1)
+        client.sendto(b"drive +", ipaddr)
+        time.sleep(1.0)
     elif keyboard.is_pressed('a'):
-            client.sendto(b"turn ++", ipaddr)
-            time.sleep(0.1)
+        client.sendto(b"turn ++", ipaddr)
+        time.sleep(1.0)
     elif keyboard.is_pressed('d'):
         client.sendto(b"turn +-", ipaddr)
-        time.sleep(0.1)
+        time.sleep(1.0)
     elif keyboard.is_pressed('s'):
-            client.sendto(b"drive -", ipaddr)
-            time.sleep(0.1)
+        client.sendto(b"drive -", ipaddr)
+        time.sleep(1.0)
     elif keyboard.is_pressed('q'):
         client.sendto(b"turn -+", ipaddr)
-        time.sleep(0.1)
+        time.sleep(1.0)
     elif keyboard.is_pressed('e'):
         client.sendto(b"turn --", ipaddr)
-        time.sleep(0.1)
+        time.sleep(1.0)
