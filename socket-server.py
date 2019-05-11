112de2a45e157d13dd5b35612f735c42ec4e7bf8
Owen Adrian <akrivus@gmail.com>
2019-05-11 15:24:05 -0500

    Downgraded to Python 3.4

--- socket-server.py
+++ socket-server.py
@@ -8,22 +8,22 @@ while 1 > 0:
     data, addr = server.recvfrom(7)
     if data == 'turn ++':
         os.system('''sh ./set-car-vector.sh w a''')
-        print(f"{addr}: Turn left!")
+        print("{}: Turn left!".format(addr))
     if data == 'turn +-':
         os.system('''sh ./set-car-vector.sh w d''')
-        print(f"{addr}: Turn right!")
+        print("{}: Turn right!".format(addr))
     if data == 'turn -+':
         os.system('''sh ./set-car-vector.sh s a''')
-        print(f"{addr}: Turn left!")
+        print("{}: Turn left!".format(addr))
     if data == 'turn --':
         os.system('''sh ./set-car-vector.sh s d''')
-        print(f"{addr}: Turn right!")
+        print("{}: Turn right!".format(addr))
     if data == 'drive +':
         os.system('''sh ./set-car-vector.sh w _''')
-        print(f"{addr}: Go forwards!")
+        print("{}: Go forwards!".format(addr))
     if data == 'drive -':
         os.system('''sh ./set-car-vector.sh s _''')
-        print(f"{addr}: Go backwards!")
+        print("{}: Go backwards!".format(addr))
     if data == 'brake !':
         os.system('''sh ./set-car-vector.sh _ _''')
-        print(f"{addr}: Brake!")
+        print("{}: Brake!".format(addr))
