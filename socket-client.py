9afaff5e8fd1f9475e1178e686f59a1fd9c34bbe
Owen Adrian <akrivus@gmail.com>
2019-05-11 15:40:44 -0500

    Added debug signals.

--- socket-client.py
+++ socket-client.py
@@ -7,15 +7,15 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while 1 > 0:
     if keyboard.is_pressed('w'):
         if keyboard.is_pressed('a'):
-            client.sendto('turn ++', (ipaddr, 4404))
+            client.sendto("turn ++".encode('utf-8'), (ipaddr, 4404))
         elif keyboard.is_pressed('d'):
-            client.sendto('turn +-', (ipaddr, 4404))
+            client.sendto("turn +-".encode('utf-8'), (ipaddr, 4404))
         else:
-            client.sendto('drive +', (ipaddr, 4404))
+            client.sendto("drive +".encode('utf-8'), (ipaddr, 4404))
     elif keyboard.is_pressed('s'):
         if keyboard.is_pressed('a'):
-            client.sendto('turn -+', (ipaddr, 4404))
+            client.sendto("turn -+".encode('utf-8'), (ipaddr, 4404))
         elif keyboard.is_pressed('d'):
-            client.sendto('turn --', (ipaddr, 4404))
+            client.sendto("turn --".encode('utf-8'), (ipaddr, 4404))
         else:
-            client.sendto('drive -', (ipaddr, 4404))
+            client.sendto("drive -".encode('utf-8'), (ipaddr, 4404))
--- socket-server.py
+++ socket-server.py
@@ -6,24 +6,18 @@ server.bind(("0.0.0.0", 4404))
 
 while 1 > 0:
     data, addr = server.recvfrom(7)
+    print("{}: data".format(addr))
     if data == 'turn ++':
-        os.system('''sh ./set-car-vector.sh w a''')
         print("{}: Turn left!".format(addr))
     if data == 'turn +-':
-        os.system('''sh ./set-car-vector.sh w d''')
         print("{}: Turn right!".format(addr))
     if data == 'turn -+':
-        os.system('''sh ./set-car-vector.sh s a''')
         print("{}: Turn left!".format(addr))
     if data == 'turn --':
-        os.system('''sh ./set-car-vector.sh s d''')
         print("{}: Turn right!".format(addr))
     if data == 'drive +':
-        os.system('''sh ./set-car-vector.sh w _''')
         print("{}: Go forwards!".format(addr))
     if data == 'drive -':
-        os.system('''sh ./set-car-vector.sh s _''')
         print("{}: Go backwards!".format(addr))
     if data == 'brake !':
-        os.system('''sh ./set-car-vector.sh _ _''')
         print("{}: Brake!".format(addr))
