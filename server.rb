a270ef196024bd9e41e3e6fe59464e61cdc71432
Owen Adrian <akrivus@gmail.com>
2019-05-29 14:57:31 -0500

    Added SoX for additional processing.

--- server.rb
+++ server.rb
@@ -87,5 +87,5 @@ def update
     end
 end
 
-Thread.new { system('nc -n 127.0.0.1 -klu -p 7055 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 44100') }
+Thread.new { system('nc -n 127.0.0.1 -klu -p 7055 | sox - -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 44100') }
 Thread.new { update }
