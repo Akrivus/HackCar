253144622df4052ab5e924578d568bae808cca2b
Owen Adrian <akrivus@gmail.com>
2019-05-29 15:37:54 -0500

    Automixing car vector data.

--- set-car-vector.sh
+++ set-car-vector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sox -m ./states/"$1".wav ./states/"$2".wav -t wav -r 48k -b 16 - repeat 10 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000
+sox -m ./states/"$1".wav ./states/"$2".wav -t wav - | sox  -t wav - -t wav -r 48k -b 16 - repeat 10 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000
