4c111ef477403e078e95bcab28511a6867168602
Owen Adrian <akrivus@gmail.com>
2019-05-29 15:15:30 -0500

    Moved sound server to separate script.

--- server.rb
+++ server.rb
@@ -93,5 +93,5 @@ def update
     end
 end
 
-Thread.new { system('nc -klu -p 6996 | sox - -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000') }
+Thread.new { system('sh ./udp-sound.sh') }
 Thread.new { update }
--- set-car-vector.sh
+++ set-car-vector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat ./states/"$1".wav | nc -q0 -u 127.0.0.1 6996 -
+cat ./states/"$1".wav | nc -u 127.0.0.1 6996 -
