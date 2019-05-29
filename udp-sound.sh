04e6c41b74452e0274ab60f263769777b4bd242f
Owen Adrian <akrivus@gmail.com>
2019-05-29 15:16:19 -0500

    Fixed shell error.

--- udp-sound.sh
+++ udp-sound.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-while true; nc -klu -p 6996 | sox - -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000; done
+while true; do nc -klu -p 6996 | sox - -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000; done
