34ef4d96163d774eef3515cb62c6e2a315bd6cbf
Owen Adrian <akrivus@gmail.com>
2019-05-29 13:44:44 -0500

    Attempting a more RESTful approach.

--- server.rb
+++ server.rb
@@ -4,74 +4,71 @@ require 'thread'
 set :bind, '0.0.0.0'
 set :port,  80
 
+VELOCITIES = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  0],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1]
+]
 CAR_MUTEX = Mutex.new()
 CAR = {
-    turn: :reset,
-    move: :reset,
-    dist:  0,
-    posx:  0,
-    posy:  0
+    velx: 0,
+    vely: 0,
+    posx: 0,
+    posy: 0
 }
 
 def update
-    CAR_MUTEX.synchronize do
-        case CAR[:move]
-        when :forward
-            system('sh ./set-car-vector.sh forward >/dev/null')
-        when :reverse
-            system('sh ./set-car-vector.sh reverse >/dev/null')
+    loop do
+        CAR_MUTEX.synchronize do
+            CAR[:posx] += CAR[:velx]
+            CAR[:posy] += CAR[:vely]
+            case CAR[:vely]
+            when -1
+                system('sh ./set-car-vector.sh reverse >/dev/null')
+            when  1
+                system('sh ./set-car-vector.sh forward >/dev/null')
+            end
+            case CAR[:velx]
+            when -1
+                system('sh ./set-car-vector.sh left >/dev/null')
+            when  1
+                system('sh ./set-car-vector.sh right >/dev/null')
+            end
         end
-        case CAR[:turn]
-        when :right
-            system('sh ./set-car-vector.sh right >/dev/null')
-        when :left
-            system('sh ./set-car-vector.sh left >/dev/null')
+        sleep 0.03
+    end
+end
+
+post 'vel/:x/:y' do
+    CAR_MUTEX.synchronize do
+        tuple = [params['x'].to_i, params['y'].to_i]
+        if VELOCITIES.include? tuple
+            CAR[:velx] = tuple[0]
+            CAR[:vely] = tuple[1]
         end
     end
-    sleep 0.03
 end
-def move(vel)
+put  'vel/x/:x'  do
     CAR_MUTEX.synchronize do
-        CAR[:move] = vel
+        tuple = [params['x'].to_i, CAR[:vely]]
+        if VELOCITIES.include? tuple
+            CAR[:velx] = tuple[0]
+        end
     end
 end
-def turn(dir)
+put  'vel/y/:y'  do
     CAR_MUTEX.synchronize do
-        CAR[:turn] = dir
+        tuple = [CAR[:velx], params['y'].to_i]
+        if VELOCITIES.include? tuple
+            CAR[:vely] = tuple[1]
+        end
     end
-end
-
-put '/turn/right' do
-    turn :right
-end
-put '/turn/reset' do
-    turn :reset
-end
-put '/turn/left' do
-    turn :left
-end
-put '/turn/*' do
-
-end
-
-put '/move/forward' do
-    move :forward
-end
-put '/move/reset' do
-    move :reset
-end
-put '/move/reverse' do
-    move :reverse
-end
-put '/move/*' do
-    
-end
-
-get '/turn' do
-
-end
-get '/move' do
-
 end
 
 Thread.new { update }
