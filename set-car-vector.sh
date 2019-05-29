a86320da88a8982c2327db4d279a09c26778cbc2
Owen Adrian <akrivus@gmail.com>
2019-05-29 15:28:31 -0500

    Whoops.

--- set-car-vector.sh
+++ set-car-vector.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sox ./states/forward.wav ./states/right.wav -m - | sox - -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000
+sox -m ./states/forward.wav ./states/right.wav - | sox - -t wav -r 48k -b 16 - repeat 1 | csdr convert_i16_f | csdr gain_ff 1 | csdr dsb_fc | sudo ../rpitx/rpitx -i - -m IQFLOAT -f 27.145e3 -s 48000
